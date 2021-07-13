0549ad312e480f31a46f0cf072a422a5efafa83a
ArthurNNN <nikitinarthur@gmail.com>
2021-07-13 15:48:15 +0200

    fix imports

--- rest-api-h2/src/main/java/com/example/employee/error/EmployeeNotFoundException.java
+++ rest-api-h2/src/main/java/com/example/employee/error/EmployeeNotFoundException.java
@@ -2,8 +2,6 @@ package com.example.employee.error;
 
 public class EmployeeNotFoundException extends RuntimeException {
 
-
-
 	public EmployeeNotFoundException( int id) {
         super("Employee id not found : " + id);
     }
